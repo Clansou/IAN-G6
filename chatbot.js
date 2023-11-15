b44ac6efd5691a9bb313889922bc5fb75eec44f3
Clansou <clement.souplet78@gmail.com>
2023-11-15 16:28:48 +0100

    add button to listen text + func

--- chatbot.js
+++ chatbot.js
@@ -48,11 +48,29 @@ sendButton.addEventListener("click", function () {
     if (message !== '') {
         var chatMessages = document.getElementById('chatMessages');
         var messageDiv = document.createElement('div');
-        messageDiv.textContent = message;
+        var messageP = document.createElement('p');
+        var messageBut = document.createElement('button');
+        messageP.textContent = message;
+        messageBut.value = message;
+        messageBut.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z"/></svg>`;
+        messageBut.addEventListener("click",function(){
+                const utterThis = new SpeechSynthesisUtterance(messageBut.value);
+                utterThis.pitch = 1;
+                utterThis.rate = 1;
+                synth.speak(utterThis);
+              
+        })
+        messageDiv.classList.add("messageDiv");
+        messageDiv.appendChild(messageP);
+        messageDiv.appendChild(messageBut);
         chatMessages.appendChild(messageDiv);
         inputElement.value = '';
     }
 })
+
+
+const selectedOption = "Microsoft Paul - French (France)"
+const synth = window.speechSynthesis;
 document.body.appendChild(newDiv)
 document.body.appendChild(chat)
 
